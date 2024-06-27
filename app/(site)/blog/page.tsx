869ce7fe41cf600e37eb3ff0313be1ee916c43f5
Hazzy <derpsy@gmail.com>
2024-06-28 01:18:12 +0800

    scrapped homepage

--- app/(site)/blog/page.tsx
+++ app/(site)/blog/page.tsx
@@ -1,12 +1,12 @@
 import Link from "next/link";
 import { Suspense } from "react";
 
-import Avatar from "./avatar";
-import CoverImage from "./cover-image";
-import DateComponent from "./date";
-import MoreStories from "./more-stories";
-import Onboarding from "./onboarding";
-import PortableText from "./portable-text";
+import Avatar from "../avatar";
+import CoverImage from "../cover-image";
+import DateComponent from "../date";
+import MoreStories from "../more-stories";
+import Onboarding from "../onboarding";
+import PortableText from "../portable-text";
 
 import type { HeroQueryResult, SettingsQueryResult } from "@/sanity.types";
 import * as demo from "@/sanity/lib/demo";
---
+++ app/(site)/page.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import { Suspense } from "react";
+
+import Avatar from "./avatar";
+import CoverImage from "./cover-image";
+import DateComponent from "./date";
+import MoreStories from "./more-stories";
+import Onboarding from "./onboarding";
+import PortableText from "./portable-text";
+
+import type { HeroQueryResult, SettingsQueryResult } from "@/sanity.types";
+import * as demo from "@/sanity/lib/demo";
+import { sanityFetch } from "@/sanity/lib/fetch";
+import { heroQuery, settingsQuery } from "@/sanity/lib/queries";
+
+
+export default async function Page() {
+  const [settings, heroPost] = await Promise.all([
+    sanityFetch<SettingsQueryResult>({
+      query: settingsQuery,
+    }),
+    sanityFetch<HeroQueryResult>({ query: heroQuery }),
+  ]);
+
+  return (
+    <div className="container mx-auto px-5">
+      Hello world!
+    </div>
+  );
+}
