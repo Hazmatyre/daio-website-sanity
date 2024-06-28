af578198d4fbfe0f9ac75cce696e7b0e39f57551
Hazzy <derpsy@gmail.com>
2024-06-28 14:25:30 +0800

    aligned hero

--- components/blocks/Layout90.Hero.tsx
+++ components/blocks/Layout90.Hero.tsx
@@ -24,8 +24,8 @@ export const Layout90 = (props: Layout90Props) => {
   } as Props;
   return (
     <section className="px-[5%] py-16 md:pb-24 lg:pb-28">
-      <div className="container max-w-xl grid grid-cols-1 items-start justify-between gap-x-12 gap-y-8 md:grid-cols-2 md:gap-x-12 md:gap-y-8 lg:gap-x-20">
-        <h1 className="text-6xl font-serif font-bold leading-[1.2] md:text-6xl lg:text-8xl xl:text-10xl whitespace-pre-line tracking-normal">{heading}</h1>
+      <div className="container xl:max-w-xl grid grid-cols-1 items-start justify-between gap-x-12 gap-y-8 md:grid-cols-2 md:gap-x-12 md:gap-y-8 lg:gap-x-20">
+        <h1 className="text-6xl font-serif font-bold leading-[1.2] md:text-6xl lg:text-8xl xl:text-10xl whitespace-pre-line tracking-normal flex md:justify-end">{heading}</h1>
         <div className="">
           <p className="md:text-md max-md:text-justify">{description}</p>
           <div className="flex mt-8 gap-x-3 justify-start">
