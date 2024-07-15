d8ac209daef071eb1765173ceaace19bec76352f
Hazzy <derpsy@gmail.com>
2024-07-15 15:09:54 +0800

    gave primary buttons an outline for contrast

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -9,11 +9,11 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        default: "bg-primary text-primary-foreground hover:bg-primary/90 border",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
-          "border border-input bg-background hover:bg-primary hover:text-primary-foreground hover:border-primary",
+          "border border-input bg-background hover:bg-primary hover:text-primary-foreground",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         accent: "bg-accent text-accent-foreground hover:bg-accent/90",
