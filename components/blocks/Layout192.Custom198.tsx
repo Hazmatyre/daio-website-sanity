54f5e9304b4eac8cc12740f3fe230a2b89c74bf8
Hazzy <derpsy@gmail.com>
2024-07-31 15:29:44 +0800

    changed color on layout192

--- components/blocks/Layout192.Custom198.tsx
+++ components/blocks/Layout192.Custom198.tsx
@@ -29,7 +29,7 @@ export const Layout192 = (props: Layout192Props) => {
     ...props,
   } as Props;
   return (
-    <header className="px-[5%] py-16 md:py-24 lg:py-28 bg-brand-50">
+    <header className="px-[5%] py-16 md:py-24 lg:py-28">
       <div className="container">
         <div className="grid grid-cols-1 gap-y-12 md:grid-cols-2 md:items-center md:gap-x-12 lg:gap-x-20">
           <div className="order-2 md:order-1 rounded-lg overflow-hidden≈">
