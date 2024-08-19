dbdbd613cac621a706f7444208e242ceab7364f6
Hazzy <derpsy@gmail.com>
2024-08-19 12:50:09 +0800

    layout 7 done

--- app/(site)/mybotany/pongamia/page.tsx
+++ app/(site)/mybotany/pongamia/page.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { Suspense } from "react";
+import { ComponentPropsWithoutRef, Suspense } from "react";
 
 // import Avatar from "./avatar";
 // import CoverImage from "./cover-image";
@@ -15,10 +15,15 @@ import { heroQuery, settingsQuery } from "@/sanity/lib/queries";
 import { Layout90 } from "@/components/blocks/Layout90.Hero";
 import { Layout245 } from "@/components/blocks/Layout245.Feature";
 import { Layout7 } from "@/components/blocks/Layout7";
-import { Gallery17 } from "@/components/blocks/Gallery17.Custom";
+import { Gallery17 } from "@/components/blocks/Gallery17.Images";
 import { Contact5 } from "@/components/blocks/Contact5";
 import { Header5 } from "@/components/blocks/Header5";
 import { Layout48 } from "@/components/blocks/Layout48";
+
+import iconListItem from "/images/mybotany-pongamia/IconListItemGreen.svg"
+import { Button } from "@/components/ui/button";
+
+import img_layout7 from "/images/mybotany-pongamia/layout7-pongamia-seeds-in-hand.webp"
 
 
 export default async function Page() {
@@ -31,13 +36,14 @@ export default async function Page() {
 
   //todo: citation component
   //todo: apply citations throughout the page
+  //todo: check that all CTA's link up
 
   return (
     <>
       <Header5 /> {/* //todo Setup buttons, optimise image */}
       <Layout48 />
-      {/* //todo  redo layout7' colors */}
-      <Layout7 />
+      {/* //done redo layout7' colors */}
+      <Layout7 {...PropsLayout7} />
       {/* //todo <Layout458/> */}
       {/* //todo <Layout417/> */}
       {/* Below Similar to layout 15 //todo content */}
@@ -56,4 +62,36 @@ export default async function Page() {
       {/* // * Remember to use layout 352 for timeline! */}
     </>
   );
+}
+
+const PropsLayout7: ComponentPropsWithoutRef<typeof Layout7> = {
+  tagline: "Cheaper than fossil fuels.",
+  heading: "The energy crisis solved.",
+  className: "text-background bg-[#241c00] [&_div:has(>img)]:border-brand-naplesYellow [&_div:has(>img)]:border",
+  classNameHeaders: "text-primary",
+  description:
+    <>Pongamia seeds can be used to produce bio-fuel through the extraction of its oils[1], or bio-coke by crushing husks of the plant into cubes and can used similar to coal[1].
+      <span>test</span></>,
+  subHeadings: [
+    {
+      icon: iconListItem,
+      title: "As good a pure diesel with less CO2 emissions.",
+      description:
+        <>Studies have shown that when blended with other fuel, it is as efficient as pure diesel[cite]. When combusted, it shows lower harmful emissions such as carbon monoxide and smoke[cite].</>
+    },
+    {
+      icon: iconListItem,
+      title: "Already used as aviation fuel by the Japanese government.",
+      description:
+        <>Japan is among the first to use Pongamia as fuel for aviation. Other countries are soon to follow as crude oil becomes more scarce and expensive.</>
+    }
+  ],
+  buttons: [
+    <a key={1}><Button>Partner With Us</Button></a>,
+    <a key={2}><Button className="dark">Pongamia In Malaysia</Button></a>
+  ],
+  image: {
+    src: img_layout7,
+    alt: "Closeup of open hand presenting several Pongamia seeds."
+  }
 }
--- components/blocks/Layout7.tsx
+++ components/blocks/Layout7.tsx
@@ -7,6 +7,7 @@ import imgPollution from '/images/pexels-ellocofish-12906459.jpg'
 import iconRecycle from '/images/PhRecycleLight.svg'
 import iconEnergy from '/images/CarbonEnergyRenewable.svg'
 import Link from "next/link";
+import { cn } from "@/lib/utils";
 
 type ImageProps = {
   src: string | StaticImport;
@@ -16,37 +17,39 @@ type ImageProps = {
 type SubHeadingProps = {
   icon?: StaticImport
   title: string;
-  description: string;
+  description: React.ReactNode;
 };
 
-type Props = {
+type Props = HTMLDivElement & {
   tagline: string;
   heading: string;
-  description: string;
+  description: React.ReactNode;
   subHeadings: SubHeadingProps[];
   buttons: React.ReactNode[];
   image: ImageProps;
+  classNameHeaders: string;
+  
 };
 
 export type Layout7Props = React.ComponentPropsWithoutRef<"section"> & Partial<Props>;
 
 export const Layout7 = (props: Layout7Props) => {
-  const { tagline, heading, description, buttons, image, subHeadings } = {
+  const { tagline, heading, description, buttons, image, subHeadings, className, classNameHeaders } = {
     ...Layout7Defaults,
     ...props,
-  } as Props;
+  };
   return (
-    <section className="px-[5%] py-16 md:py-24 lg:py-28">
+    <section className={cn("px-[5%] py-16 md:py-24 lg:py-28", className)}>
       <div className="container">
         <div className="grid grid-cols-1 gap-y-12 md:grid-flow-row md:grid-cols-2 md:items-center md:gap-x-12 lg:gap-x-20">
           <div>
             <p className="mb-3 type-regular font-semibold md:mb-4">{tagline}</p>
-            <h2 className="mb-5 type-mobile-h2 lg:type-desktop-h2 font-bold md:mb-6">{heading}</h2>
+            <h2 className={cn("mb-5 type-mobile-h2 lg:type-desktop-h2 font-bold md:mb-6", classNameHeaders)}>{heading}</h2>
             <p className="mb-6 md:mb-8 type-regular font-medium md:type-medium">{description}</p>
             <div className="grid grid-cols-1 gap-6 py-2 md:grid-cols-2">
-              {subHeadings.map((subHeading, index) => (
+              {subHeadings?.map((subHeading, index) => (
                 <div key={index} className="no-overflow-anchoring">
-                  <h3 className="mb-3 type-mobile-h6 font-bold leading-[1.4] md:mb-4 md:type-desktop-h6 flex gap-x-3">
+                  <h3 className={cn("mb-3 type-mobile-h6 font-bold leading-[1.4] md:mb-4 md:type-desktop-h6 flex gap-x-3", classNameHeaders)}>
                     {subHeading.icon && <Image alt="" src={subHeading.icon} className="size-8"></Image>}
                     {subHeading.title}
                   </h3>
@@ -58,18 +61,20 @@ export const Layout7 = (props: Layout7Props) => {
               {buttons}
             </div>
           </div>
-          <div className="rounded-3xl overflow-hidden shadow-xxlarge">
-            <Image
-              src={image.src}
-              className="w-full object-cover"
-              alt={image.alt || ""}
-              quality={25}
-              sizes="(max-width: 768px) 90vw, (max-width: 1200px) 50vw, 33vw"
-              width={338}
-              height={422}
-              loading="lazy"
-            />
-          </div>
+          {image &&
+            <div data= className="rounded-3xl overflow-hidden shadow-xxlarge">
+              <Image
+                src={image.src}
+                className="w-full object-cover"
+                alt={image.alt || ""}
+                quality={25}
+                sizes="(max-width: 768px) 90vw, (max-width: 1200px) 50vw, 33vw"
+                width={338}
+                height={422}
+                loading="lazy"
+              />
+            </div>
+          }
         </div>
       </div>
     </section>
