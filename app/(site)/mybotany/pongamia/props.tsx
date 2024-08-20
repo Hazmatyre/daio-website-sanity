3925c0acf9d33b6877eec4d33f6be83878a9a83f
Hazzy <derpsy@gmail.com>
2024-08-20 16:06:25 +0800

    added photos

--- app/(site)/mybotany/pongamia/props.tsx
+++ app/(site)/mybotany/pongamia/props.tsx
@@ -1,18 +1,15 @@
 /* eslint-disable jsx-a11y/alt-text */
 import Image, { StaticImageData } from "next/image";
-import img1 from "/images/mybio-bio-toilet/toilet-mount-fuji/bio-toilet-fujisan01.jpg"
-import img2 from "/images/mybio-bio-toilet/toilet-mount-fuji/bio-toilet-fujisan02.jpg"
-import img3 from "/images/mybio-bio-toilet/toilet-mount-fuji/bio-toilet-fujisan03.jpg"
-import img4 from "/images/mybio-bio-toilet/toilet-mount-fuji/bio-toilet-fujisan04.jpg"
-import img5 from "/images/mybio-bio-toilet/toilet-mount-fuji/bio-toilet-fujisan06.jpg"
-import img6 from "/images/mybio-bio-toilet/toilet-mount-fuji/bio-toilet-fujisan07.jpg"
-import img7 from "/images/mybio-bio-toilet/toilet-mount-fuji/bio-toilet-fujisan08.jpg"
-import img8 from "/images/mybio-bio-toilet/toilet-mount-fuji/bio-toilet-fujisan10.jpg"
-import img9 from "/images/mybio-bio-toilet/toilet-mount-fuji/bio-toilet-fujisan11.jpg"
-import img10 from "/images/mybio-bio-toilet/toilet-mount-fuji/bio-toilet-fujisan12.jpg"
-import img11 from "/images/mybio-bio-toilet/toilet-mount-fuji/bio-toilet-fujisan13.jpg"
-import img12 from "/images/mybio-bio-toilet/toilet-mount-fuji/bio-toilet-fujisan14.jpg"
-import img13 from "/images/mybio-bio-toilet/toilet-mount-fuji/bio-toilet-fujisan15.jpg"
+import img1 from "/images/mybotany-pongamia/img2.webp"
+import img2 from "/images/mybotany-pongamia/img1.webp"
+import img3 from "/images/mybotany-pongamia/img3.webp"
+import img4 from "/images/mybotany-pongamia/img4.webp"
+import img5 from "/images/mybotany-pongamia/img5.webp"
+import img6 from "/images/mybotany-pongamia/img6.webp"
+import img7 from "/images/mybotany-pongamia/img7.webp"
+import img8 from "/images/mybotany-pongamia/img8.webp"
+import img9 from "/images/mybotany-pongamia/img9.webp"
+import img10 from "/images/mybotany-pongamia/img10.webp"
 
 import { Gallery17 } from "@/components/blocks/Gallery17.Images";
 
@@ -21,85 +18,69 @@ const fujiSizes = "80vw, (min-width: 767px) 55vw, (min-width: 1000px) 42vw, (min
 
 const fujiSizesPortrait = "64vw, (min-width: 767px) 31vw, (min-width: 1000px) 23vw, (min-width: 1279px) 19vw, (min-width: 1439px) 16vw, (min-width: 1919px) 12vw"
 
+const quality = 50
+
 
 const images: React.ComponentProps<typeof Image>[] = [
   {
     src: img1,
     alt: "",
     sizes: fujiSizes,
-    quality: 10,
+    quality: quality,
   },
   {
     src: img2,
     alt: "",
     sizes: fujiSizes,
-    quality: 10,
+    quality: quality,
   },
   {
     src: img3,
     alt: "",
     sizes: fujiSizes,
-    quality: 10,
+    quality: quality,
   },
   {
     src: img4,
     alt: "",
-    sizes: fujiSizesPortrait,
-    quality: 10,
+    sizes: fujiSizes,
+    quality: quality,
   },
   {
     src: img5,
     alt: "",
-    sizes: fujiSizesPortrait,
-    quality: 10,
+    sizes: fujiSizes,
+    quality: quality,
   },
   {
     src: img6,
     alt: "",
-    sizes: fujiSizesPortrait,
-    quality: 10,
+    sizes: fujiSizes,
+    quality: quality,
   },
   {
     src: img7,
     alt: "",
     sizes: fujiSizes,
-    quality: 10,
+    quality: quality,
   },
   {
     src: img8,
     alt: "",
     sizes: fujiSizes,
-    quality: 10,
+    quality: quality,
   },
   {
     src: img9,
     alt: "",
     sizes: fujiSizes,
-    quality: 10,
+    quality: quality,
   },
   {
     src: img10,
     alt: "",
     sizes: fujiSizes,
-    quality: 10,
-  },
-  {
-    src: img11,
-    alt: "",
-    sizes: fujiSizes,
-    quality: 10,
-  },
-  {
-    src: img12,
-    alt: "",
-    sizes: fujiSizes,
-    quality: 10,
-  },
-  {
-    src: img13,
-    alt: "",
-    sizes: fujiSizes,
-    quality: 10,
+    quality: quality,
   },
 ]
 
