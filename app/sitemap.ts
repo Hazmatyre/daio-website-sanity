53555c5148a3a434e8357a23d82bd9820b1869fa
Hazzy <derpsy@gmail.com>
2024-09-07 17:01:00 +0800

    corrected sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,25 +3,25 @@ import type { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://acme.com',
+      url: 'https://daiointernational.com',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 1,
     },
     {
-      url: 'https://acme.com/about',
+      url: 'https://daiointernational.com/about',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 0.7,
     },
     {
-      url: 'https://acme.com/mybio/biotoilet',
+      url: 'https://daiointernational.com/mybio/biotoilet',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 0.9,
     },
     {
-      url: 'https://acme.com/mybotany/pongamia',
+      url: 'https://daiointernational.com/mybotany/pongamia',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 0.9,
